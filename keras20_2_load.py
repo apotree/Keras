7ff20bc1243ed91d22f6e6f4783da22ed508b877
apotree <52978976+apotree@users.noreply.github.com>
2020-01-12 02:12:02 +0900

    Update keras20_2_load.py

--- keras20_2_load.py
+++ keras20_2_load.py
@@ -6,7 +6,7 @@ import numpy as np
 
 x = np.array(range(1,101))
 y = np.array(range(1,101))
-print(x)
+print(x) 
 
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(
